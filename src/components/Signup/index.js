208ed478ef3e8d2a29bc757d61e46a53ea57e131
Mason Anders <mason.anders@gopuff.com>
2023-08-03 13:14:48 -0700

    Add error handling

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -6,24 +6,45 @@ export default function Signup() {
     const [password, setPassword] = useState('')
     const [confPass, setConfPass] = useState('')
     const [showPass, setShowPass] = useState(false)
+    const [error, setError] = useState(false)
+
+    function fieldsAreValid() {
+        return !!username && !!password && password === confPass
+    }
+
+    function submit() {
+        if (fieldsAreValid()) {
+            // Handle successful sign up
+        } else {
+            setError(true)
+        }
+    }
 
     return (
         <div className="signup-form">
             <h1>Sign up</h1>
 
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)} />
+            <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)} error={String(error && !!username)} />
             
             <label htmlFor="pass">Password</label>
-            <input type={showPass ? 'text' : 'password'} id="pass" value={password} onChange={e => setPassword(e.target.value)} />
+            <input type={showPass ? 'text' : 'password'} id="pass" value={password} onChange={e => setPassword(e.target.value)} error={String(error && (!!password || password !== confPass))} />
 
             <label htmlFor="conf-pass">Confirm Password</label>
-            <input type={showPass ? 'text' : 'password'} id="conf-pass" value={confPass} onChange={e => setConfPass(e.target.value)} />
+            <input type={showPass ? 'text' : 'password'} id="conf-pass" value={confPass} onChange={e => setConfPass(e.target.value)} error={String(error && (!!confPass || password !== confPass))} />
 
             <label htmlFor="show-pass">Show Password</label>
             <input type="checkbox" id="show-pass" checked={showPass} onChange={e => setShowPass(e.target.checked)} />
 
-            <button>Signup</button>
+            <button disabled={error && !fieldsAreValid()} onClick={submit}>Sign up</button>
+            
+            { error && (
+                <div>
+                    {!username && <p>Username is required</p>}
+                    {!(password || confPass) && <p>Password is required</p>}
+                    {password !== confPass && <p>Passwords must match</p>}
+                </div>
+            ) }
         </div>
     )
 }

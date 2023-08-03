6018bc0c9c48e37578e2f0ec16bc6f1a9e6163a8
Mason Anders <mason.anders@gopuff.com>
2023-08-03 14:44:38 -0700

    Add styling for main and welcome

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,10 +1,13 @@
-import "./welcome.css"
+import "./welcome.css";
 
 export default function Welcome({ username, onBackToSignup }) {
   return (
-    <div>
-      <p>{`Welcome, ${username}!`}</p>
-      <p>Thank you for signing up.</p>
+    <div id="welcome">
+      <h2>{`Welcome, ${username}!`}</h2>
+      <p>
+        Thank you for signing up. This is a prototype feature that has no
+        further functionality.
+      </p>
       <button onClick={onBackToSignup}>Go back to signup</button>
     </div>
   );

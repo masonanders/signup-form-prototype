90fb3534356cf3d865227fce375bb3f4e3235193
Mason Anders <mason.anders@gopuff.com>
2023-08-03 12:46:18 -0700

    Enable "Show password" checkbox functionality

--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -13,10 +13,10 @@ export default function Signup() {
             <input type="text" id="username" value={username} onChange={e => setUsername(e.target.value)} />
             
             <label htmlFor="pass">Password</label>
-            <input type="password" id="pass" value={password} onChange={e => setPassword(e.target.value)} />
+            <input type={showPass ? 'text' : 'password'} id="pass" value={password} onChange={e => setPassword(e.target.value)} />
 
             <label htmlFor="conf-pass">Confirm Password</label>
-            <input type="password" id="conf-pass" value={confPass} onChange={e => setConfPass(e.target.value)} />
+            <input type={showPass ? 'text' : 'password'} id="conf-pass" value={confPass} onChange={e => setConfPass(e.target.value)} />
 
             <label htmlFor="show-pass">Show Password</label>
             <input type="checkbox" id="show-pass" checked={showPass} onChange={e => setShowPass(e.target.checked)} />
